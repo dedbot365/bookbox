07055aaa4c8bc4b24484395c80e400ebf072781b
kibby2 <sar1439@my.londonmet.ac.uk>
2025-04-16 19:03:22 +0545

    everything setup success. discuss: versions, how to view database, connstring

---
+++ bookbox/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using bookbox.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookbox.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
