89c75846486b98a99fadce422ed275cb65d094a6
dedbot365 <poudelsubigyan73@gmail.com>
2025-05-04 10:02:52 +0545

    Data being updated

--- bookbox/Entities/Users.cs
+++ bookbox/Entities/Users.cs
@@ -1,7 +1,10 @@
-﻿namespace bookbox.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookbox.Entities
 {
     public class Users
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
