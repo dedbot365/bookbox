8f0afb12c3a5e33f8900d37836db82edbcededdb
kibby2 <sar1439@my.londonmet.ac.uk>
2025-05-06 11:03:28 +0545

    Genre enum

---
+++ Bookbox/Constants/Genre.cs
@@ -0,0 +1,20 @@
+namespace Bookbox.Constants
+{
+    public enum Genre
+    {
+        Fiction = 1,
+        NonFiction = 2,
+        Mystery = 3,
+        Fantasy = 4,
+        ScienceFiction = 5,
+        Romance = 6,
+        Horror = 7,
+        Thriller = 8,
+        Biography = 9,
+        History = 10,
+        Poetry = 11,
+        SelfHelp = 12,
+        Children = 13,
+        YoungAdult = 14
+    }
+}
