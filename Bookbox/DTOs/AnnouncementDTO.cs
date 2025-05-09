b41c7c874ab83a8e70378aed8079166ae851a4db
Subigyan Paudel <50463900+dedbot365@users.noreply.github.com>
2025-05-09 21:35:57 +0545

    Update and rename AnnouncementDto.cs to AnnouncementDTO.cs

--- Bookbox/DTOs/AnnouncementDTO.cs
+++ Bookbox/DTOs/AnnouncementDTO.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Bookbox.DTOs
 {
-    public class AnnouncementDto
+    public class AnnouncementDTO
     {
         public Guid? AnnouncementId { get; set; }
         
@@ -12,24 +12,16 @@ namespace Bookbox.DTOs
         public string Title { get; set; } = string.Empty;
         
         [Required(ErrorMessage = "Content is required")]
-        [Display(Name = "Description")]
         public string Content { get; set; } = string.Empty;
         
         [Required(ErrorMessage = "Start date is required")]
         [Display(Name = "Start Date")]
-        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; } = DateTime.UtcNow;
         
         [Display(Name = "End Date")]
-        [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; }
         
+        [Display(Name = "Active")]
         public bool IsActive { get; set; } = true;
-        
-        public Guid? UserId { get; set; }
-        
-        public string? UserName { get; set; }
-        
-        public DateTime LastModified { get; set; } = DateTime.UtcNow;
     }
 }
